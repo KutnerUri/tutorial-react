6549f36c5683594c489cb02fc9b6791faf0178e5
Oded Reuveny <oded@cocycles.com>
2017-10-22 18:41:08 +0300

    example projects for react-tutorial with button component

---
+++ export-button/src/ui/button.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+
+/**
+ * @description styled button component
+ * @param {function} click - invoked function onclick
+ * @param {string} text - text that appears in the button
+ *
+ *
+ */
+
+class Button extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state={hover: false}
+
+    this.toggleHover = this.toggleHover.bind(this)
+  }
+
+  toggleHover() {
+    this.setState({hover: !this.state.hover})
+  }
+
+  showAlert() {
+    alert("Button clicked");
+  }
+
+
+  render() {
+    let style = {
+      width: "18%",
+      height:"40px",
+      display: "inline-block",
+      fontFamily: "Apple",
+      boxShadow: "4px 5px 5px #888888",
+      backgroundColor: this.state.hover ? "rgb(119, 148, 162)" : "rgba(161, 188, 202, 0.34)"
+    }
+    return (
+      <button style={style} onMouseEnter={this.toggleHover} onMouseLeave={this.toggleHover} onClick={this.showAlert}>Example Button</button>
+    )
+  }
+
+}
+
+export default Button;
